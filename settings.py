3db2a2874f5a17751bfe4b5f11431c38a157baa0
melferink <m.elferink@umcutrecht.nl>
2017-04-13 14:13:00 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+## Settings used in rsync_to_rdisc.py # 
+
+# mail finished transfer 
+
+email_from='[<email>]'
+
+finished_mail=[
+              '[<email>'
+              ]
+
+owner_dic={'<user>':'<email>','<user>':'<email>' } 
+
