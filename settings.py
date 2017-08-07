204c7916c23d99032ca019dc2fb5054b5eb94b5b
melferink <m.elferink@umcutrecht.nl>
2017-08-07 16:02:51 +0200

    added several paths to settings file

--- settings.py
+++ settings.py
@@ -1,12 +1,17 @@
 ## Settings used in rsync_to_rdisc.py # 
 
+wkdir="/hpc/cog_bioinf/diagnostiek/processed/Upload/"
+log="Rsync_Dx.log"
+errorlog="Rsync_Dx.errorlog"
+temperror="/hpc/cog_bioinf/diagnostiek/processed/Upload/temp.error"
+
 # mail finished transfer 
 
-email_from='[<email>]'
+email_from='<email>'
 
 finished_mail=[
-              '[<email>'
+              '<email>'
               ]
 
-owner_dic={'<user>':'<email>','<user>':'<email>' } 
+owner_dic={'<user1>':'<email1>','<user2>':'<email2>'}
 
