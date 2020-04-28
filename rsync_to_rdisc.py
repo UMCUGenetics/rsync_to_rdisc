bc8987681c71ba8ba353c8a4050e4cd7cbc69c27
melferink <m.elferink@umcutrecht.nl>
2020-04-28 15:40:33 +0200

    code refactor + IAP NF comparible

--- rsync_to_rdisc.py
+++ rsync_to_rdisc.py
@@ -1,235 +1,240 @@
 #! /usr/bin/env python
-import sys, os, re, math, time, commands, datetime
-from os.path import join, isdir, isfile, split
-from os import system, walk, listdir, chdir, rename, stat,lstat
+import sys
+import os
+#import re
+#import math
+#import time
+import commands
+import datetime
+#from os import stat
+from pwd import getpwuid
+#from os.path import join, isdir, isfile, split
+from os.path import join, isfile, split
+#from os import system, walk, listdir, chdir, rename, stat,lstat
 from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 import smtplib
 import mimetypes
-import settings
-import datetime, time
-from os import stat
-from pwd import getpwuid
 import socket
+
+import settings
+
 ####################################################################################################################################
 # AUTHOR:       M.G. Elferink
 # DATE:         29-04-2014
-# Purpose:	Automatic sync Dx-run folders within to /data/DIT-bgarray/
-#		Exome, GENEPANEL, and NIPT data
-#		
-#		This script is excecuted with a cronjob (each hour), or can be manually runned by executing ./sync_to_rdisc.py 
-#		However, only for user melferink!	
-#
+# Purpose:    Automatic sync Dx-run folders within to /data/DIT-bgarray/
 ####################################################################################################################################
 
 def find_owner(filename):
-    return getpwuid(lstat(filename).st_uid).pw_name
+    return getpwuid(os.lstat(filename).st_uid).pw_name
 
-def make_mail(filename,state):
-	if state[0] == "ok": 
-	        try:
-	                email_to=[settings.owner_dic[find_owner(filename)]]
-	        except:
-     			email_to = settings.finished_mail
-		""" Send complete mail """
-	        subject = 'COMPLETED: Transfer to BGarray has succesfully completed for {}'.format(filename)
-        	text = "<html><body><p>"+'Transfer to BGarray has succesfully completed for {}'.format(filename)+"</p></body></html>"
-	elif state[0] == "error": # send error mail to owner of cronjob/mount
-                try:
-                        email_to=[settings.owner_dic[find_owner(filename)]]
-                        email_to += [settings.finished_mail]
-                except:
-                        email_to = settings.finished_mail  
-		subject = 'ERROR: transfer to BGarray has not completed for {}'.format(filename)
-                text = "<html><body><p>"+'Transfer to BGarray has not been completed for {}'.format(filename)+"</p></body></html>"
-	elif state[0] == "lost":
-                email_to=[]
-                try:
-                    for item in settings.owner_dic:
-                        email_to+=[settings.owner_dic[item]]
-                except:
-                    email_to = settings.finished_mail
-                subject = 'ERROR: mount to BGarray for {}'.format(socket.gethostname())
-                text = "<html><body><p>"+'Mount to BGarray is lost for {}'.format(socket.gethostname())+"</p></body></html>"
-	elif state[0] == "cleanup":
-		try:
-                        email_to=[settings.owner_dic[find_owner(filename)]]
-                except:
-                        email_to = settings.finished_mail
-                """ Send complete mail """
-                subject = 'PLEASE CLEAN-UP YOUR RUN {} !'.format(filename)
-                text = "<html><body><p>"+'Data not transferred to BGarray. Run {}'.format(filename)+"</p></body></html>"
-        send_email(settings.email_from, email_to, subject, text)
+def make_mail(filename, state):
+    if state[0] == "ok": 
+        try:
+            email_to = [settings.owner_dic[find_owner(filename)]]
+        except:
+             email_to = settings.finished_mail
+        """ Send complete mail """
+        subject = 'COMPLETED: Transfer to BGarray has succesfully completed for {}'.format(filename)
+        text = "<html><body><p>" + 'Transfer to BGarray has succesfully completed for {}'.format(filename) + "</p></body></html>"
+    elif state[0] == "error": # send error mail to owner of cronjob/mount
+        try:
+            email_to = [settings.owner_dic[find_owner(filename)]]
+            email_to += [settings.finished_mail]
+        except:
+            email_to = settings.finished_mail  
+        subject = 'ERROR: transfer to BGarray has not completed for {}'.format(filename)
+        text = "<html><body><p>" + 'Transfer to BGarray has not been completed for {}'.format(filename) + "</p></body></html>"
+    elif state[0] == "lost":
+        email_to = []
+        try:
+            for item in settings.owner_dic:
+                email_to += [settings.owner_dic[item]]
+        except:
+            email_to = settings.finished_mail
+        subject = 'ERROR: mount to BGarray for {}'.format(socket.gethostname())
+        text = "<html><body><p>" + 'Mount to BGarray is lost for {}'.format(socket.gethostname()) + "</p></body></html>"
+    elif state[0] == "notcomplete":
+        try:
+            email_to = [settings.owner_dic[find_owner(filename)]]
+        except:
+            email_to = settings.finished_mail
+        """ Send complete mail """
+        subject = 'IAP analysis not complete (no workflow.done file). Run = {} !'.format(filename)
+        text = "<html><body><p>" + 'Data not transferred to BGarray. Run {}'.format(filename) + "</p></body></html>"
+    send_email(settings.email_from, email_to, subject, text)
 
 def send_email(sender, receivers, subject, text, attachment=None):
-	mail = MIMEMultipart()
-	mail['Subject'] = subject
-	mail['From'] = sender
-	mail['To'] = ';'.join(receivers)
-	if attachment:
-	       	filename = attachment.split('/')[-1]
-	       	fp = open(attachment, 'rb')
-	       	ctype, encoding = mimetypes.guess_type(attachment)
-		if ctype is None or encoding is not None:
-        		# No guess could be made, or the file is encoded (compressed), so
-        		# use a generic bag-of-bits type.
-        		ctype = 'application/octet-stream'
-		maintype, subtype = ctype.split('/', 1)
-		msg = MIMEBase(maintype, subtype)
-		msg.set_payload(fp.read())
-		fp.close()
-		# Encode the payload using Base64
-		encoders.encode_base64(msg)
-		msg.add_header('Content-Disposition', 'attachment', filename=filename)
-		mail.attach(msg)
-	msg = MIMEText(text,'html')
-	mail.attach(msg)
-	m = smtplib.SMTP('smtp-open.umcutrecht.nl')
-	m.sendmail(sender, receivers, mail.as_string())
-	m.quit()
+    mail = MIMEMultipart()
+    mail['Subject'] = subject
+    mail['From'] = sender
+    mail['To'] = ';'.join(receivers)
+    if attachment:
+        filename = attachment.split('/')[-1]
+        fp = open(attachment, 'rb')
+        ctype, encoding = mimetypes.guess_type(attachment)
+        if ctype is None or encoding is not None:
+            # No guess could be made, or the file is encoded (compressed), so
+            # use a generic bag-of-bits type.
+            ctype = 'application/octet-stream'
+        maintype, subtype = ctype.split('/', 1)
+        msg = MIMEBase(maintype, subtype)
+        msg.set_payload(fp.read())
+        fp.close()
+        # Encode the payload using Base64
+        encoders.encode_base64(msg)
+        msg.add_header('Content-Disposition', 'attachment', filename=filename)
+        mail.attach(msg)
+    msg = MIMEText(text, 'html')
+    mail.attach(msg)
+    m = smtplib.SMTP('smtp-open.umcutrecht.nl')
+    m.sendmail(sender, receivers, mail.as_string())
+    m.quit()
 
-def check(action,run,processed,run_org,folder): ## perform actual Rsync command, and check if no errors.
-	print "Rsync run:" +str(run)
-	os.system(action)
-	error= commands.getoutput("wc -l "+str(temperror))
-        if int(error.split()[0]) == 0: ## check if there are errors in de temporary error file. If so, do not include runid in transferred_runs.txt
-		if processed == 1:
-	        	os.system("echo " + str(run)+"_"+str(folder)+">> "+str(wkdir)+"transferred_runs.txt")
-		os.system("echo \"\n>>> No errors detected <<<\" >>/data/DIT-bgarray/"+str(log))
-        	os.system("rm "+str(temperror))
-		print "no errors"
-                make_mail(str(wkdir)+str(folder)+"/"+str(run), ["ok"])
-		return "ok"
- 	else:
-		action="echo \">>>\""+str(run)+"_"+str(folder)+" \"errors detected in Processed data transfer, not added to completed files <<<\""
-                os.system(action + ">>/data/DIT-bgarray/"+ str(log))
-		print "errors, check errorlog file"
-		make_mail(run, ["error"])
-		return "error"
+def check(action, run, processed, run_org, folder): ## perform actual Rsync command, and check if no errors.
+    #print "Rsync run:" + str(run)
+    print "Rsync run:{}".format(run)
+    os.system(action)
+    #error = commands.getoutput("wc -l " + str(temperror))
+    error = commands.getoutput("wc -l {}".format(temperror))
+    if int(error.split()[0]) == 0: ## check if there are errors in de temporary error file. If so, do not include runid in transferred_runs.txt
+        if processed == 1:
+            #os.system("echo " + str(run) + "_" + str(folder) + ">> " + str(wkdir) + "transferred_runs.txt")
+            os.system("echo {run}_{folder} >> {wkdir}/transferred_runs.txt".format(
+                run = run,
+                folder = folder,
+                wkdir = wkdir
+                ))
 
+        #os.system("echo \"\n>>> No errors detected <<<\" >>/data/DIT-bgarray/" + str(log))
+        #os.system("rm " + str(temperror))
+        os.system("echo \"\n>>> No errors detected <<<\" >>/data/DIT-bgarray/{}".format(log))
+        os.system("rm {}".format(temperror))
+        print "no errors"
+        make_mail(str(wkdir) + str(folder) + "/" + str(run), ["ok"])
+        return "ok"
+    else:
+        #action = "echo \">>>\"" + str(run) + "_" + str(folder) + " \"errors detected in Processed data transfer, not added to completed files <<<\""
+        action = "echo \">>>\" {run}_{folder} \"errors detected in Processed data transfer, not added to completed files <<<\" >> /data/DIT-bgarray/{log}".format(
+            run = run,
+            folder = folder ,
+            log = log
+            ) 
+        #os.system(action + ">>/data/DIT-bgarray/" + str(log))
+        os.sytem(action)
+        print "errors, check errorlog file"
+        make_mail(run, ["error"])
+        return "error"
 
-def sync(action1,action2,item,processed,folder): ## check if run has been (succesfully) synced before. If so, skip, else perform sync
-	if folder == "RAW_data_MIPS":
-		rundir = []
-		sequencedir = os.listdir(str(item))
-		for dir in sequencedir: ## loop into sequencers
-			runs=os.listdir(str(item)+"/"+str(dir))
-			for run in runs:
-				rundir +=[str(dir)+"/"+str(run)] 
-	else:
-		rundir = os.listdir(str(item))
+def sync(action1, action2, folder, processed, item): ## check if run has been (succesfully) synced before. If so, skip, else perform sync
+    if item == "RAW_data_MIPS":
+        rundir = []
+        sequencedir = os.listdir(str(folder))
+        for sequencefolder in sequencedir: ## loop into sequencers
+            #runs = os.listdir(str(folder) + "/" + str(dir))
+            runs = os.listdir("{}/{}".format(folder,sequencefolder))
+            for run in runs:
+                #rundir += [str(sequencefolder) + "/" + str(run)] 
+                rundir += ["{}/{}".format(sequencefolder,run)]
+    else:
+        rundir = os.listdir(str(folder))
 
-	state ="hold"
-	run_list=[]
-       	for run in rundir:
-        	try: ## check runid in dictionary
-			dic[str(run)+"_"+str(folder)]
-            	except:
-			raw_file=''
-			if folder == "Exomes" or folder == "Genepanels":
-				raw_file=commands.getoutput("find -L "+str(item)+"/"+str(run)+" -maxdepth 1 -iname \"*.raw_variants.vcf\"")
-			if folder == "RAW_data_MIPS":
-				done_file='{}/{}/{}/{}'.format(wkdir,folder,run,'TransferDone.txt')
-				if not isfile(done_file):
-					pass
-				else:
-					action= str(action1)+str(run)+"\""+str(action2)
-                                	os.system("echo \"\n#########\nDate: "+str(date)+"\nRun_folder: "+str(run)+ "\" >>/data/DIT-bgarray/" +str(log))
-                                	file_org="" ## dummy
-                                	state = check(action,run,processed,file_org,folder)
-                                	run_list+=[run]
-			else:
-				if not isfile(raw_file):
-					action= str(action1)+str(run)+"\""+str(action2)
-					os.system("echo \"\n#########\nDate: "+str(date)+"\nRun_folder: "+str(run)+ "\" >>/data/DIT-bgarray/" +str(log))
-					file_org="" ## dummy
-					state = check(action,run,processed,file_org,folder)
-					run_list+=[run]
-				else:
-					make_mail(str(wkdir)+str(folder)+"/"+str(run), ["cleanup"])
-	return state,run_list
+    state = "hold"
+    run_list = []
+    for run in rundir:
+        try: ## check runid in dictionary
+            #dic[str(run) + "_" + str(item)]
+            dic["{}_{}".format(run,item)]
+        except:
+            if item == "RAW_data_MIPS":
+                done_file = '{}/{}/{}/{}'.format(wkdir, item, run,'TransferDone.txt')
+                if not isfile(done_file):
+                    pass
+                else:
+                    #action = str(action1) + "/" + str(run) + " " + str(action2)
+                    action = "{}/{} {}".format(action1,run,action2)
+                    #os.system("echo \"\n#########\nDate: " + str(date) + "\nRun_folder: " + str(run) + "\" >>/data/DIT-bgarray/" + str(log))
+                    os.system("echo \"\n#########\nDate: {date} \nRun_folder: {run} \" >>/data/DIT-bgarray/{log}".format(
+                        date = date,
+                        run = run,
+                        log = log
+                        ))
+                    file_org = "" ## dummy
+                    state = check(action, run, processed, file_org, item)
+                    run_list += [run]
+            else:
+                if item == "Exomes" and not os.path.isfile("{0}/{1}/workflow.done".format(folder, run)):  ## If IAP run is not completed.
+                    #make_mail(str(wkdir) + str(item) + " " + str(run), ["notcomplete"])
+                    make_mail("{}/{} {}".format(wkdir, item, run), ["notcomplete"])
+                else:
+                    #action = str(action1) + "/" + str(run) + " " + str(action2)
+                    action = "{}/{} {}".format(action1, run, action2)
+                    #os.system("echo \"\n#########\nDate: " + str(date) + "\nRun_folder: " + str(run) + "\" >>/data/DIT-bgarray/" + str(log))
+                    os.system("echo \"\n#########\nDate: {date} \nRun_folder: {run} \" >>/data/DIT-bgarray/{log}".format( 
+                        date = date,
+                        run = run,
+                        log = log
+                        ))  
+                    file_org = "" ## dummy
+                    state = check(action, run, processed, file_org, item)
+                    run_list += [run]
+    return state, run_list
 
 ### START ###
 
 # check if mount to BGarray intact. If not, restore
 if os.path.exists("/data/DIT-bgarray/Illumina/") == True:
-	pass
+    pass
 else:
-	print "Mount is lost. Please contact M. Elferink for restore"
-	make_mail("mount", ["lost"])	
-	sys.exit()
+    print "Mount is lost. Please contact M. Elferink for restore"
+    make_mail("mount", ["lost"])    
+    sys.exit()
 
-wkdir=settings.wkdir
-dic={}
+wkdir = settings.wkdir
+dic = {}
 try:
-	runs=open(str(wkdir)+"transferred_runs.txt","r").readlines()
-	for line in runs:
-        	dic[line.rstrip()]=""
-
+    runs = open(str(wkdir) + "transferred_runs.txt", "r").readlines()
+    for line in runs:
+        dic[line.rstrip()] = ""
 except:
-	new_file=open(str(wkdir)+"transferred_runs.txt","w")
-	new_file.close()
+    new_file = open(str(wkdir) + "transferred_runs.txt", "w")
+    new_file.close()
 
 # if running exit, else create transfer.running file and continue
-running=str(wkdir)+"/transfer.running"
+running = str(wkdir) + "/transfer.running"
 if os.path.isfile(running):
     sys.exit()
 else:
-    os.system("touch "+running)
-
+    os.system("touch " + running)
 
 ## Rsync folders ##
 
-log =settings.log
-errorlog=settings.errorlog
-temperror=settings.temperror
-
-date= str(datetime.datetime.now()).split(".")[0]
+log = settings.log
+errorlog = settings.errorlog
+temperror = settings.temperror
+date = str(datetime.datetime.now()).split(".")[0]
 lib_dir = os.walk(str(wkdir)).next()[1]
+
 for item in lib_dir:
-	processed=1
-	state=[""] 
-	if item== "Exomes":
-		folder="Exomes"
-		action1="rsync -rahuL --stats --exclude '*fastq.gz' \""+str(wkdir)+"Exomes/"
-		action2=" /data/DIT-bgarray/Illumina/Exomes/ 1>>/data/DIT-bgarray/"+str(log)+" 2>>/data/DIT-bgarray/" +str(errorlog)+" 2>"+str(temperror)
-		state=sync(action1,action2,str(wkdir)+str(item),processed,folder)
+    processed = 1
+    state = [""]
+    if item not in settings.folder_list:
+        print "Folder does not exist"
+    else:
+        #folder = str(wkdir) + "/" + str(item)
+        folder = "{}/{}".format(wkdir,item)
+        #action1 = "rsync -rahuL --stats " + str(folder)
+        action1 = "rsync -rahuL --stats {}".format(folder)
+        #action2 = " /data/DIT-bgarray/Illumina/" + str(item) + "/ 1>>/data/DIT-bgarray/" + str(log) + " 2>>/data/DIT-bgarray/" + str(errorlog) + " 2>" + str(temperror) 
+        action2 = " /data/DIT-bgarray/Illumina/{item}/ 1>> /data/DIT-bgarray/{log} 2>> /data/DIT-bgarray/{errorlog} 2> {temperror}".format(
+            item = item,
+            log = log,
+            errorlog = errorlog,
+            temperror = temperror
+            ) 
+        state = sync(action1, action2, folder, processed, item)
 
-	elif item == "TRANSFER":
-		folder="TRANSFER"
-		action1="rsync -rahuL --stats \""+ str(wkdir)+"TRANSFER/"
-       		action2=" /data/DIT-bgarray/TRANSFER/ 1>>/data/DIT-bgarray/"+str(log)+" 2>>/data/DIT-bgarray/" +str(errorlog)+" 2>"+str(temperror)
- 		processed=1
-		state=sync(action1,action2,str(wkdir)+str(item),processed,folder)
-	
-        elif item == "MIPS":
-                folder = "MIPS"
-                action1="rsync -rahuL --stats \""+ str(wkdir)+"MIPS/"
-                action2=" /data/DIT-bgarray/Illumina/MIPS 1>>/data/DIT-bgarray/"+str(log)+" 2>>/data/DIT-bgarray/" +str(errorlog)+" 2>"+str(temperror)
-                processed=1
-                state=sync(action1,action2,str(wkdir)+str(item),processed,folder)
-	
-	elif item == "RAW_data_MIPS":
-		folder = "RAW_data_MIPS"
-                action1="rsync -rahuL --stats \""+ str(wkdir)+"RAW_data_MIPS/"
-                action2=" /data/DIT-bgarray/RAW_data/MIPS 1>>/data/DIT-bgarray/"+str(log)+" 2>>/data/DIT-bgarray/" +str(errorlog)+" 2>"+str(temperror)
-                processed=1
-                state=sync(action1,action2,str(wkdir)+str(item),processed,folder)
-
-	elif item == "RAW_data":
-		folder="RAW_data"
-                action1="rsync -rahuL --stats \""+ str(wkdir)+"RAW_data/"
-                action2=" /data/DIT-bgarray/RAW_data/BACKUP_TEMP/ 1>>/data/DIT-bgarray/"+str(log)+" 2>>/data/DIT-bgarray/" +str(errorlog)+" 2>"+str(temperror)
-                processed=1
-                state=sync(action1,action2,str(wkdir)+str(item),processed, folder)
-
-        elif item == "Genomes":
-                folder = "Genomes"
-                action1="rsync -rahuL --stats \""+ str(wkdir)+"Genomes/"
-                action2=" /data/DIT-bgarray/Illumina/Genomes 1>>/data/DIT-bgarray/"+str(log)+" 2>>/data/DIT-bgarray/" +str(errorlog)+" 2>"+str(temperror)
-                processed=1
-                state=sync(action1,action2,str(wkdir)+str(item),processed,folder)
-
-os.system("rm "+running)
+#os.system("rm " + running)
+os.system("rm {}".format(running))
