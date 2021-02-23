22b5dee1860a761e02a7ee670b7f2a5382572e81
melferink <m.elferink@umcutrecht.nl>
2021-02-23 12:28:48 +0100

    included bgarray parameter, and changed bgarray path

--- settings.py
+++ settings.py
@@ -1,18 +1,21 @@
 """Settings used in rsync_to_rdisc.py"""
-wkdir="/hpc/diaggen/data/upload/"
-log="Rsync_Dx.log"
-errorlog="Rsync_Dx.errorlog"
-temperror="/hpc/diaggen/data/upload/temp.error"
+wkdir = "/hpc/diaggen/data/upload/"
+log = "Rsync_Dx.log"
+errorlog = "Rsync_Dx.errorlog"
+temperror = "/hpc/diaggen/data/upload/temp.error"
+bgarray = "/data/diaggen/DIT-bgarray" 
 
-folder_dic = {"Exomes":"/data/DIT-bgarray/Illumina/Exomes","TRANSFER":"/data/DIT-bgarray/TRANSFER","MIPS":"/data/DIT-bgarray/Illumina/MIPS","RAW_data_MIPS":"/data/DIT-bgarray/RAW_data/MIPS","RAW_data":"/data/DIT-bgarray/RAW_data/BACKUP_TEMP","Genomes":"/data/DIT-bgarray/Illumina/Genomes"}
+folder_dic = {"Exomes":"/data/diaggen/DIT-bgarray/Illumina/Exomes", 
+    "TRANSFER":"/data/diaggen/DIT-bgarray/TRANSFER", 
+    "MIPS":"/data/diaggen/DIT-bgarray/Illumina/MIPS", 
+    "RAW_data_MIPS":"/data/diaggen/DIT-bgarray/RAW_data/MIPS", 
+    "RAW_data":"/data/diaggen/DIT-bgarray/RAW_data/BACKUP_TEMP", 
+    "Genomes":"/data/diaggen/DIT-bgarray/Illumina/Genomes"}
 
 """Mail finished transfer"""
-email_from='<email>'
+email_from = '<email>'
 
-finished_mail=['<email>'
-               ]
+finished_mail = ['<email>']
 
-owner_dic={'<username1>':'<email1>',
-           '<username2>':'<email2>',
-          }
+owner_dic = {'<username1>':'<email1>','<username2>':'<email2>'}
 
