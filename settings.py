6d76c9b47649fb9e2e99ef683053965534c9161d
melferink <melferink@vm010317.ds.umcutrecht.nl>
2022-01-28 11:03:16 +0100

    new setup

--- settings.py
+++ settings.py
@@ -1,23 +1,28 @@
 """Settings used in rsync_to_rdisc.py"""
-wkdir = "/hpc/diaggen/data/upload/"
+
+wkdir = "/diaggen/data/upload/"
+temperror = "{}temp.error".format(wkdir)
 log = "Rsync_Dx.log"
 errorlog = "Rsync_Dx.errorlog"
-temperror = "/hpc/diaggen/data/upload/temp.error"
-bgarray = "/data/diaggen/DIT-bgarray" 
+bgarray = "/mnt/bgarray/"
 
+host_keys = ''
+server = ''
+user = ''
+
+## folder_dic: Value is 1 = hpc location, 2 = transfer location, 3 = file to be checked, 4 = True/False, in which True = continue (i.e. if raw data transfer is running), False = send error mail (i.e. is workflow.done is missing)
 folder_dic = {
-    "Exomes":"/data/diaggen/DIT-bgarray/Illumina/Exomes", 
-    "TRANSFER":"/data/diaggen/DIT-bgarray/TRANSFER", 
-    "MIPS":"/data/diaggen/DIT-bgarray/Illumina/MIPS", 
-    "RAW_data_MIPS":"/data/diaggen/DIT-bgarray/RAW_data/MIPS", 
-    "RAW_data":"/data/diaggen/DIT-bgarray/RAW_data/BACKUP_TEMP", 
-    "Genomes":"/data/diaggen/DIT-bgarray/Illumina/Genomes"
-    }
+    "Exomes":["/hpc/diaggen/data/upload/Exomes/", "{}Illumina/Exomes/".format(bgarray), "workflow.done", "False"],
+    "Genomes":["/hpc/diaggen/data/upload/Genomes/", "{}Illumina/Genomes/".format(bgarray), "workflow.done", "False"],
+    "MIPS":["/hpc/diaggen/data/upload/MIPS/", "{}Illumina/MIPS/".format(bgarray), "workflow.done", "False"],   
+    "RAW_data_MIPS_nextseq_umc01":["/hpc/diaggen/data/upload/RAW_data_MIPS/nextseq_umc01/", "{}RAW_data/MIPS/".format(bgarray), "TransferDone.txt", "True"],
+    "RAW_data_MIPS_nextseq_umc02":["/hpc/diaggen/data/upload/RAW_data_MIPS/nextseq_umc02/", "{}RAW_data/MIPS/".format(bgarray), "TransferDone.txt", "True"],
+    "TRANSFER":["/hpc/diaggen/data/upload/TRANSFER/", "{}TRANSFER/".format(bgarray), "", "False"],
+    "RAW_data":["/hpc/diaggen/data/upload/RAW_data/", "{}RAW_data/BACKUP_TEMP/".format(bgarray), "", "False"],
+}
 
 """Mail finished transfer"""
-email_from = '<email>'
+email_from = ''
 
-finished_mail = '<email>'
-
-owner_dic = {'<username1>':'<email1>','<username2>':'<email2>'}
+email_to = ['', '']
 
