917ba1502c5a8886b8d736cc4585abd3c143eadc
melferink <melferink@vm010317.ds.umcutrecht.nl>
2022-02-02 14:20:19 +0100

    added nested list for folder_dic

--- settings.py
+++ settings.py
@@ -23,49 +23,49 @@ email_to = ['', '']
 # folder_dic: value items
 # 1 = hpc location,
 # 2 = transfer location,
-# 3 = file to be checked,
+# 3 = list of one or more files to be checked (note that all files need to be present),
 # 4 = True/False, in which True = continue without mail, and False = send error mail and stop
 folder_dic = {
-    "Exomes": [
-        "/hpc/diaggen/data/upload/Exomes/",
-        "{}Illumina/Exomes/".format(bgarray),
-        "workflow.done",
-        "False"
-    ],
-    "Genomes": [
-        "/hpc/diaggen/data/upload/Genomes/",
-        "{}Illumina/Genomes/".format(bgarray),
-        "workflow.done",
-        "False"
-    ],
-    "MIPS": [
-        "/hpc/diaggen/data/upload/MIPS/",
-        "{}Illumina/MIPS/".format(bgarray),
-        "workflow.done",
-        "False"
-    ],
-    "RAW_data_MIPS_nextseq_umc01": [
-        "/hpc/diaggen/data/upload/RAW_data_MIPS/nextseq_umc01/",
-        "{}RAW_data/MIPS/".format(bgarray),
-        "TransferDone.txt",
-        "True"
-    ],
-    "RAW_data_MIPS_nextseq_umc02": [
-        "/hpc/diaggen/data/upload/RAW_data_MIPS/nextseq_umc02/",
-        "{}RAW_data/MIPS/".format(bgarray),
-        "TransferDone.txt",
-        "True"
-    ],
-    "TRANSFER": [
-        "/hpc/diaggen/data/upload/TRANSFER/",
-        "{}TRANSFER/".format(bgarray),
-        "",
-        "False"
-    ],
-    "RAW_data": [
-        "/hpc/diaggen/data/upload/RAW_data/",
-        "{}RAW_data/BACKUP_TEMP/".format(bgarray),
-        "",
-        "False"
-    ],
+    "Exomes": {
+        "input":"/hpc/diaggen/data/upload/Exomes/",
+        "output":"{}Illumina/Exomes/".format(bgarray),
+        "files_required":["workflow.done"],
+        "continue_without_email":"False"
+    },
+    "Genomes": {
+        "input":"/hpc/diaggen/data/upload/Genomes/",
+        "output":"{}Illumina/Genomes/".format(bgarray),
+        "files_required":["workflow.done"],
+        "continue_without_email":"False"
+    },
+    "MIPS": {
+        "input":"/hpc/diaggen/data/upload/MIPS/",
+        "output":"{}Illumina/MIPS/".format(bgarray),
+        "files_required":["workflow.done"],
+        "continue_without_email":"False"
+    },
+    "RAW_data_MIPS_nextseq_umc01": {
+        "input":"/hpc/diaggen/data/upload/RAW_data_MIPS/nextseq_umc01/",
+        "output":"{}RAW_data/MIPS/".format(bgarray),
+        "files_required":["TransferDone.txt"],
+        "continue_without_email":"True"
+    },
+    "RAW_data_MIPS_nextseq_umc02": {
+        "input":"/hpc/diaggen/data/upload/RAW_data_MIPS/nextseq_umc02/",
+        "output":"{}RAW_data/MIPS/".format(bgarray),
+        "files_required":["TransferDone.txt"],
+        "continue_without_email":"True"
+    },
+    "TRANSFER": {
+        "input":"/hpc/diaggen/data/upload/TRANSFER/",
+        "output":"{}TRANSFER/".format(bgarray),
+        "files_required":[""],
+        "continue_without_email":"False"
+    },
+    "RAW_data": {
+        "input":"/hpc/diaggen/data/upload/RAW_data/",
+        "output":"{}RAW_data/BACKUP_TEMP/".format(bgarray),
+        "files_required":[""],
+        "continue_without_email":"False"
+    },
 }
